ced09ce37c5b272042d5988d19eeb781cc143782
esatgozcu <esatgozcu@gmail.com>
2023-11-14 10:08:24 +0300

    User input component has been added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import Header from "./components/Header"
+import UserInput from "./components/UserInput"
 
 function App() {
   return (
-    <Header/>
+    <>
+      <Header />
+      <UserInput />
+    </>
   )
 }
 
