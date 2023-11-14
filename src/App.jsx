7756a4cd1c82eed4cebf7c849dc52667869c9a2a
esatgozcu <esatgozcu@gmail.com>
2023-11-14 11:00:51 +0300

    Result component has been added and connected to data.

--- src/App.jsx
+++ src/App.jsx
@@ -10,19 +10,24 @@ function App() {
     expectedReturn: 6,
     duration: 10
   })
+
+  const inputIsValid = userInput.duration >= 1
+
   function handleChange(inputIdentifier, newValue) {
     setUserInput(prevUserInput => {
       return {
         ...prevUserInput,
-        [inputIdentifier]: newValue
+        [inputIdentifier]: +newValue
       }
     })
   }
   return (
     <>
       <Header />
-      <UserInput onChange={handleChange} userInput={userInput}/>
-      <Results input={userInput}/>
+      <UserInput onChange={handleChange} userInput={userInput} />
+      {!inputIsValid && <p className = 'center'>Please enter a duration greater than zero.</p>}
+      {inputIsValid && <Results input={userInput} 
+      />}
     </>
   )
 }
